725cceb9f81f527d296a0d062f1d1615f8596d6a
AhmedShantti <20220848@student.alazhar.edu.ps>
2026-05-31 20:07:47 +0300

    Fix: Restore TableForeignKey import in MenuItemAddonsTable migration

--- POS/src/database/migrations/1779600000000-CreateMenuItemAddonsTable.ts
+++ POS/src/database/migrations/1779600000000-CreateMenuItemAddonsTable.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { MigrationInterface, QueryRunner, Table, TableForeignKey } from 'typeorm';
 
 export class CreateMenuItemAddonsTable1779600000000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
